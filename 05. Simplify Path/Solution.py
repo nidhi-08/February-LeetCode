8c6c7b0d96233f7bc36594f9851f9633b14c9ced
Nidhi Anil Khare <55029966+nidhi-08@users.noreply.github.com>
2021-02-06 12:01:33 +0530

    Solution 5th Feb

---
+++ 05. Simplify Path/Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack =[]
+        for elem in path.split('/'):
+            if stack and elem == '..':
+                stack.pop()
+            elif elem not in ['.','','..']:
+                stack.append(elem)
+            
+        return '/'+'/'.join(stack)
+            
+        
